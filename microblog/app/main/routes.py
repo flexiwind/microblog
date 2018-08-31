cf993ee766a6cd32932641a7b9c2e12d567b6764
tangnengfeng <tangnengfeng@shijiyunhe.com>
2018-08-31 17:58:32 +0800

    JavaScript魔法

--- microblog/app/main/routes.py
+++ microblog/app/main/routes.py
@@ -86,8 +86,14 @@ def user(username):
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, current_app.config['POST_PER_PAGE'], False)
     next_url = url_for('main.user', username=user.username, page=posts.next_num) if posts.has_next else None
     prev_url = url_for('main.user', username=user.username, page=posts.prev_num) if posts.has_prev else None
-    return render_template('user.html', user=user,
-                           posts=posts.items, next_url=next_url, prev_url=prev_url)
+    return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
+
+
+@bp.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user_popup.html', user=user)
 
 
 @bp.route('/edit_profile', methods=['GET', 'POST'])
